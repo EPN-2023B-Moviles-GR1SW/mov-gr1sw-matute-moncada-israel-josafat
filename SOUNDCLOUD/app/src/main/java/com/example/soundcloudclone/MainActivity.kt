8050ba3e209677f9149c7ab57f82c1d0bd872701
jzafvt771 <ijmm7171@gmail.com>
2024-02-10 19:54:19 -0500

    SC TOP MENU COMPLETED

--- SOUNDCLOUD/app/src/main/java/com/example/soundcloudclone/MainActivity.kt
+++ SOUNDCLOUD/app/src/main/java/com/example/soundcloudclone/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.soundcloudclone
 import android.content.Intent
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import com.google.android.material.snackbar.Snackbar
 
@@ -11,6 +12,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         irActividad(Greeting::class.java)
+
+        var btnUpgrade = findViewById<Button>(R.id.upgrade_btn)
+        btnUpgrade.setOnClickListener{
+            mostrarSnackbar("UPGRADE")
+        }
+
+        var btnCast = findViewById<Button>(R.id.cast_btn)
+        btnCast.setOnClickListener{
+            mostrarSnackbar("CAST")
+        }
+
+        var btnUpload = findViewById<Button>(R.id.upload_btn)
+        btnUpload.setOnClickListener{
+            mostrarSnackbar("UPLOAD")
+        }
+
+        var btnMsg = findViewById<Button>(R.id.msg_btn)
+        btnMsg.setOnClickListener{
+            mostrarSnackbar("MESSAGE")
+        }
+
+        val btnNotif = findViewById<Button>(R.id.notif_btn)
+        btnNotif.setOnClickListener{
+            mostrarSnackbar("NOTIFICATION")
+        }
+
     }
 
     fun irActividad (
